651cab63353000c6af5d2d1bbdd7f2357792ed77
CorainChicago <chaysmagan@gmail.com>
2016-12-01 22:56:40 -0600

    add inital donut table

--- js/main.js
+++ js/main.js
@@ -2,7 +2,13 @@ requirejs([
   'jquery',
   'knockout'
   ], function($, ko) {
-    'use strict'
-    console.log('hi');
+    var initial = [
+        { name: 'Vanilla Glaze', type: 'yeast', cost: '$3.44', rating: 'good', description: 'A good basic donut'} ];
+   
+   var ViewModel = function(donuts){
+        self.donuts = ko.observableArray(donuts);
+   }
+
+   ko.applyBindings(new ViewModel(initial))
 
 });
